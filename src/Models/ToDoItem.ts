8e87df644492288df0108d563f29ae401b102152
Georg Perhofer <>
2023-01-03 14:53:35 +0100

    f

--- src/Models/ToDoItem.ts
+++ src/Models/ToDoItem.ts
@@ -4,8 +4,7 @@ import { Group, IGroup } from 'Models/Group'
 import { ToDoList } from 'Models/ToDoList'
 import * as system from 'System/index'
 
-export interface IToDoItem
-{
+export interface IToDoItem {
     id: string
     name: string
     groupId: string
@@ -16,22 +15,18 @@ export interface IToDoItem
 
 }
 
-export class ToDoItem
-{
+export class ToDoItem {
     eventAggregator: EventAggregator
 
     id: string
 
     private _name: string
-    public get name(): string
-    {
+    public get name(): string {
         return this._name
     }
-    public set name(v: string)
-    {
+    public set name(v: string) {
         this._name = v
-        if (this.toDoList && this.modified)
-        {
+        if (this.toDoList && this.modified) {
             this.modified = new Date()
             this.toDoList.hasUserChanges = true
             this.eventAggregator.publish('ToDoItemChanged', this)
@@ -39,18 +34,15 @@ export class ToDoItem
     }
 
     private _done: boolean
-    public get done(): boolean
-    {
+    public get done(): boolean {
         return this._done
     }
-    public set done(v: boolean)
-    {
+    public set done(v: boolean) {
         this._done = v
         if (this.modified && !v && this.modified.addDays(1) < new Date())
             this.usage++
 
-        if (this.toDoList && this.modified)
-        {
+        if (this.toDoList && this.modified) {
             this.modified = new Date()
             this.toDoList.hasUserChanges = true
             this.eventAggregator.publish('ToDoItemChanged', this)
@@ -66,15 +58,13 @@ export class ToDoItem
 
     toDoList: ToDoList
 
-    constructor(toDoList?: ToDoList, name = '', doBaseInitialization = true)
-    {
+    constructor(toDoList?: ToDoList, name = '', doBaseInitialization = true) {
         this.eventAggregator = Container.instance.get(EventAggregator)
 
         this.toDoList = toDoList
         this.name = name
 
-        if (doBaseInitialization)
-        {
+        if (doBaseInitialization) {
             this.id = new system.Guid().toString()
             this.name = name
             this.done = false
@@ -84,9 +74,8 @@ export class ToDoItem
         }
     }
 
-    static map(toDoList: ToDoList, item: IToDoItem): ToDoItem
-    {
-        let toDo = new ToDoItem(toDoList, item.name, false)
+    static map(toDoList: ToDoList, item: IToDoItem): ToDoItem {
+        const toDo = new ToDoItem(toDoList, item.name, false)
         toDo.id = item.id
         toDo.done = item.done
         toDo.usage = item.usage
