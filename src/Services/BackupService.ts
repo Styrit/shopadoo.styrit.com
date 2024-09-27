30d9bc52b6aaee48f3c6273bd661cfd834d1feab
GP <>
2024-09-27 20:33:02 +0200

    loginDismissed support

--- src/Services/BackupService.ts
+++ src/Services/BackupService.ts
@@ -12,6 +12,7 @@ import { StorageService } from './StorageService'
 @autoinject
 export class BackupService {
     private logger = getLogger('BackupService')
+    private loginDismissed = false
 
     error: unknown
 
@@ -21,7 +22,7 @@ export class BackupService {
         private appService: AppService
     ) {
         this.appService.onVisibilityChanged.on(async visible => {
-            if (!settingsService.settings.autoBackup)
+            if (!settingsService.settings.autoBackup && !this.loginDismissed)
                 return
             if (!visible || this.error) {
                 if (this.settingsService.settings.lastAutoBackupDate.addDays(4) <= new Date()) {
@@ -29,6 +30,10 @@ export class BackupService {
                     let loggedIn = false
                     try {
                         loggedIn = await this.settingsService.storageProvider.authService.login(false)
+                        if (!loggedIn) {
+                            this.loginDismissed = true
+                            this.logger.info('Login dismissed.')
+                        }
                     }
                     catch (error) {
                         this.logger.info('Error on login.')
